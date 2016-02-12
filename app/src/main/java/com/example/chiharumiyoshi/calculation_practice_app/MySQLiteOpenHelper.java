29c00b472d9e292da1a3a32d3a1bb0fb5ce40050
chiharum <m.chiharu0611@gmail.com>
2016-02-12 18:59:34 +0900

    2016.2.12 Forwardで間違えた問題を重点的に出すプログラムを作りたいと思います。

--- app/src/main/java/com/example/chiharumiyoshi/calculation_practice_app/MySQLiteOpenHelper.java
+++ app/src/main/java/com/example/chiharumiyoshi/calculation_practice_app/MySQLiteOpenHelper.java
@@ -10,6 +10,7 @@ public class MySQLiteOpenHelper extends SQLiteOpenHelper {
     static final int DATABASE_VERSION = 1;
     static final String QUESTIONS_TABLE_NAME = "questions";
     static final String RECORD_TABLE_NAME = "records";
+    static final String FORWARD_TABLE_NAME = "forward";
 
     public MySQLiteOpenHelper(Context context){
 
@@ -19,6 +20,7 @@ public class MySQLiteOpenHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase database){
         database.execSQL("create table " + QUESTIONS_TABLE_NAME + " (_id integer primary key autoincrement not null, question_number integer not null, number1 integer not null, number2 integer not null, correct_answer integer not null, answer integer)");
         database.execSQL("create table " + RECORD_TABLE_NAME + " (_id integer primary key autoincrement not null, date integer not null, calculation_kind text not null, correct_rate integer not null, time_per_a_question integer not null)");
+        database.execSQL("create table " + FORWARD_TABLE_NAME + " (_id integer primary key autoincrement not null, calculation_kind integer not null, number1 integer not null, number2 integer not null, correct_time integer not null, total_time integer not null");
     }
 
     public void onUpgrade(SQLiteDatabase database, int oldVersion, int newVersion){
